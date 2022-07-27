59dd57fad8b630f0054d4aedbaab2373fb869a63
Bartosz Pindel <kaladin112@gmail.com>
2022-07-27 12:01:42 +0200

    set nav, small changes in theme object

---
+++ script/q&a.js
@@ -0,0 +1,112 @@
+const gothicTheme = {
+  themeName: "Gothic",
+  questionsEasy: [
+    {
+      question: "Kto spuszcza łomot Bezimiennemu po zesłaniu do kolonii?",
+      answers: [
+        { text: "Bullit", correct: true },
+        { text: "Diego", correct: false },
+        { text: "Y'berion", correct: false },
+        { text: "Wrzód", correct: false },
+      ],
+    },
+    {
+      question: "Która osoba z podanych NIE jest przyjacielem Bezimiennego?",
+      answers: [
+        { text: "Cipher", correct: true },
+        { text: "Lee", correct: false },
+        { text: "Milten", correct: false },
+        { text: "Diego", correct: false },
+      ],
+    },
+    {
+      question:
+        "Kto jest dowódcą paladynów w Górniczej Dolinie, a kto w Khorinis?",
+      answers: [
+        { text: "Khorinis - Lord Hagen, GD - Garond", correct: true },
+        { text: "Khorinis - Lord Garond, GD - Hagen", correct: false },
+        { text: "Khorinis - Lord Rhobar, GD - Orik", correct: false },
+        { text: "Khorinis - Lord Orik, GD - Rhobar", correct: false },
+      ],
+    },
+    {
+      question:
+        "Które z podanych krain możemy odwiedzić podczas gameplayu Gothica 3",
+      answers: [
+        { text: "Myrtana, Nordmar, Varant", correct: true },
+        { text: "Myrtana, Północne Ziemie, Varant", correct: false },
+        { text: "Normard, Varant, Północne Ziemie", correct: false },
+        {
+          text: "Wyspy Południowe, Północne Ziemie, Niezbadane Krainy",
+          correct: false,
+        },
+      ],
+    },
+    {
+      question: "Do oficjalnego kanonu zaliczamy, które części Gothica?",
+      answers: [
+        {
+          text: "Gothic, Gothic 2, Gothic 2: Noc Kruka, Gothic 3",
+          correct: true,
+        },
+        {
+          text: "Gothic, Gothic 2: Noc Kruka, Gothic 3",
+          correct: false,
+        },
+        {
+          text: "Gothic, Gothic 2, Gothic 3",
+          correct: false,
+        },
+        {
+          text: "Gothic, Gothic: Mroczne Tajemnice, Gothic 2, Gothic 2: Noc Kruka, Gothic 3, Gothic 3: Zmierzch Bogów",
+          correct: false,
+        },
+      ],
+    },
+    {
+      question: "Kto jest głównym antagonistą w dwóch pierwszych częściach gry",
+      answers: [
+        { text: "Śniący i Smok Ożywieniec", correct: true },
+        { text: "Śniący i Bestia ", correct: false },
+        { text: "Rhobar", correct: false },
+        { text: "Wrzód", correct: false },
+      ],
+    },
+    {
+      question: "Kto spuszcza łomot Bezimiennemu po zesłaniu do kolonii?",
+      answers: [
+        { text: "Bullit", correct: true },
+        { text: "Diego", correct: false },
+        { text: "Rhobar", correct: false },
+        { text: "Wrzód", correct: false },
+      ],
+    },
+    {
+      question: "Kto spuszcza łomot Bezimiennemu po zesłaniu do kolonii?",
+      answers: [
+        { text: "Bullit", correct: true },
+        { text: "Diego", correct: false },
+        { text: "Rhobar", correct: false },
+        { text: "Wrzód", correct: false },
+      ],
+    },
+    {
+      question: "Kto spuszcza łomot Bezimiennemu po zesłaniu do kolonii?",
+      answers: [
+        { text: "Bullit", correct: true },
+        { text: "Diego", correct: false },
+        { text: "Rhobar", correct: false },
+        { text: "Wrzód", correct: false },
+      ],
+    },
+    {
+      question: "Kto spuszcza łomot Bezimiennemu po zesłaniu do kolonii?",
+      answers: [
+        { text: "Bullit", correct: true },
+        { text: "Diego", correct: false },
+        { text: "Rhobar", correct: false },
+        { text: "Wrzód", correct: false },
+      ],
+    },
+  ],
+};
