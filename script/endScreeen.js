146f789645108d21160bb250e263d137d98678ea
Bartosz Pindel <kaladin112@gmail.com>
2022-08-25 16:24:45 +0200

    add endscreen.js, program ending screen

---
+++ script/endScreeen.js
@@ -0,0 +1,23 @@
+let guild = document.querySelector("span.guild");
+let quote = document.querySelector("blockquote.quote");
+let pointsNumber = document.querySelector("span.points");
+let aboutGuild = document.querySelector("p.aboutGuild");
+
+const endScreen = () => {
+  document.querySelector(".qAndA").classList.toggle("nvisible");
+  document.querySelector(".endView").classList.toggle("nvisible");
+  pointsNumber.textContent = points;
+  if (guildId === 1) {
+    guild.textContent = currentDiffGuilds[0].guildName;
+    quote.textContent = currentDiffGuilds[0].quoteText;
+    aboutGuild.textContent = currentDiffGuilds[0].desc;
+  } else if (guildId === 2) {
+    guild.textContent = currentDiffGuilds[1].guildName;
+    quote.textContent = currentDiffGuilds[1].quoteText;
+    aboutGuild.textContent = currentDiffGuilds[1].desc;
+  } else {
+    guild.textContent = currentDiffGuilds[2].guildName;
+    quote.textContent = currentDiffGuilds[2].quoteText;
+    aboutGuild.textContent = currentDiffGuilds[2].desc;
+  }
+};
